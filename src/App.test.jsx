e559de91741c7c141bf0a41024a8bb15f8e3d7ae
fora-heims <foraheims@gmail.com>
2022-01-24 16:26:44 -0800

    test for header image and profile name passing.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,5 +1,16 @@
-test('Should render the header', () => {
+import { screen, render } from '@testing-library/react'
+import App from './App'
+
+test('Should render the header', async () => {
+  render(<App />)
+
   // - header image
+  const headerImage = screen.getByRole('img', { name: /alchemy/i })
+  expect(headerImage).toBeInTheDocument()
+
   // - profile name
+  const profileName = await screen.findByText(/fora/i)
+  expect(profileName).toBeInTheDocument()
+
   // - Background Color (stretch)
 })
