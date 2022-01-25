d487794a1a388f5e86097c52410a6b2be35f0415
fora-heims <foraheims@gmail.com>
2022-01-24 18:02:48 -0800

    tests not finished or passing for Home component.

--- src/views/Home/Home.test.jsx
+++ src/views/Home/Home.test.jsx
@@ -1,27 +1,54 @@
+import { screen, render } from '@testing-library/react'
+import Home from './Home'
+
 const user = {
-  id: 1,
-  created_at: '2021-12-13T00:17:29+00:00',
-  name: 'Vonta',
-  avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
-  header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
-  likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
-  motto: 'Res Non Verba',
-  color: 'crimson',
+  id: 2,
+  created_at: '2022-01-24T22:01:37+00:00',
+  name: 'Fora',
+  avatar: 'https://i.pinimg.com/originals/c8/de/fb/c8defbf13c1870d0501f86143e5a7ebb.png',
+  header:
+    'https://media.istockphoto.com/photos/magical-fairytale-forest-picture-id1082411378?k=20&m=1082411378&s=612x612&w=0&h=GalnNnhN5wVYqKXee-qy8AYrdOUDxEnkJlqC8T0q9dY=',
+  likes: [
+    'Juggling',
+    'Roller Skating',
+    'Printmaking',
+    'Chemistry',
+    'Crystals',
+    'Books',
+    'Computers',
+  ],
+  motto: 'You can only be what you are.',
+  color: 'Orangered',
 }
 
 test('Should render the user profile', () => {
+  render(<Home user={user} />)
   //   - name
+  const userName = screen.getByRole('header', { name: /fora/i })
   //   - motto
+  const userMotto = screen.getByRole('paragraph', { name: /motto/i })
   //   - interests heading
+  const userInterestsHeading = screen.getByRole('header', { name: /interests/i })
   //   - avatar
+  const userAvatar = screen.getByRole('img', { name: /avatar/i })
   //   - header image
+  const userHeaderImage = screen.getByRole('img', { name: /header/i })
   //   - list of user likes
-  // - (Stretch) You can also write tests that ensures the `user` object received as a prop to the `Home` component has the following shape:
-  //   - id
-  //   - name
-  //   - avatar
-  //   - header
-  //   - likes
-  //   - motto
-  //   - color
+  // const userListOfLikes = screen.getAllByRole('listitem', {})
+
+  expect(userName).toBeInTheDocument()
+  expect(userMotto).toBeInTheDocument()
+  expect(userInterestsHeading).toBeInTheDocument()
+  expect(userAvatar).toBeInTheDocument()
+  expect(userHeaderImage).toBeInTheDocument()
+  // expect(userListOfLikes.length).toEqual(7)
 })
+
+// - (Stretch) You can also write tests that ensures the `user` object received as a prop to the `Home` component has the following shape:
+//   - id
+//   - name
+//   - avatar
+//   - header
+//   - likes
+//   - motto
+//   - color
